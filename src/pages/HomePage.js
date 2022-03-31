451acc49efb7a6510de3c9f1fabf2f40d52932a5
ardevsrc <dbeast@tuta.io>
2022-03-31 07:35:27 -0400

    Added private-route page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,37 +1,15 @@
 import React from 'react';
-import styled from 'styled-components';
-import { PageHero, StripeCheckout } from '../components';
+import { FeaturedProducts, Hero, Services, Contact } from '../components';
 
-import { useCartContext } from '../context/cart_context';
-import { Link } from 'react-router-dom';
-
-const CheckoutPage = () => {
-  const { cart } = useCartContext()
+const HomePage = () => {
   return (
     <main>
-      <PageHero title='checkout' />
-      <Wrapper className='page'>
-        {cart.length < 1 ? (
-          <div className='empty'>
-            <h2>your cart is empty</h2>
-            <Link to='/products' className='btn'>
-              fill it
-            </Link>
-          </div>
-        ) : (
-          <StripeCheckout />
-        )}
-      </Wrapper>
+      <Hero />
+      <FeaturedProducts />
+      <Services />
+      <Contact />
     </main>
   )
-};
+}
 
-const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  .empty {
-    text-align: center;
-  };
-`
-export default CheckoutPage;
+export default HomePage;
